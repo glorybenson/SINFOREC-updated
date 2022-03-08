f53650f42b7833c9f72e24e64353506d575018ee
Glory Benson <glorybenson3738@gmail.com>
2022-03-08 06:23:43 -0800

    bgt

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -14,7 +14,7 @@ return [
     */
 
     'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'sent' => 'Nous avons envoyé votre lien de réinitialisation de mot de passe par e-mail !',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",

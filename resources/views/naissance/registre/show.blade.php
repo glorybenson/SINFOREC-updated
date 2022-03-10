f6b653c7edb323eaca9e07934b1f327cef3d20d3
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 01:48:40 -0800

    hmm

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -9,17 +9,7 @@
                         <div class="row align-items-center">
                             <div class="col-md-12">
                                 <div class="d-flex align-items-center">
-                                    <h5 class="page-title">Dashboard</h5>
-                                    <ul class="breadcrumb ml-2">
-                                        <li class="breadcrumb-item"><a href="{{ route('registre') }}">Des
-                                                détails</a>
-                                        </li>
-                                        <li class="breadcrumb-item active"></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                                  
                     <div class="row">
                         <div class="col-md-12">
                             <div class="card">

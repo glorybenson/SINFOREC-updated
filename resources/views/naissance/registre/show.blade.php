b7f0db9ef9517b286787cf24b8d56179b919f8ee
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 07:47:27 -0800

    sss

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -68,7 +68,7 @@
                                         <p>Nom de famille de l'enfant</p>
                                     </div>
                                     <div class="col-9">
-                                        <p>{{$add =->get('child_info-last_name')}};</p>
+                                        <p>{$add =->get{('child_info-last_name')}}</p>
                                     </div>
                                 </div>
                                 <hr>

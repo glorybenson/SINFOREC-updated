4d5d597a69e2bd97feda36dd793b23f0e7ce75ff
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 02:00:52 -0800

    hous

---
+++ resources/views/naissance/registre/show.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+    .badge {
+        font-size: 16px;
+    }
+</style>
+<div class="row">
+    <div class="col-md-10 mx-auto">
+        <div class="mt-3">
+            <div class="content container-fluid">
+                <div class="page-header">
+                    <div class="row align-items-center">
+                        <div class="col-md-12">
+                            <div class="d-flex align-items-center">
+                                <h5 class="page-title">Dashboard</h5>
+                                <ul class="breadcrumb ml-2">
+                                    <li class="breadcrumb-item"><a href="{{ route('home') }}">Naissance</a>
+                                    </li>
+                                    <li class="breadcrumb-item active">View</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+@include('layouts.includes.filterJs')
+@endsection

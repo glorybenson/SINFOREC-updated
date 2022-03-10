976d94aa3b1cd37a842453b29b20c16f2457a7e1
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 03:50:39 -0800

    ssss

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -37,10 +37,15 @@
                                 <div class="card-body">
                                 <div class="row pr-3 pl-3">
                                     <div class="col-3">
+                                    @foreach ($add as $item)
+                                    @php
+                                        if(isset($item))
+                                            $item->values = json_decode( $item->values);
+                                    @endphp
                                         <h6>Prénom</h6>
                                     </div>
                                     <div class="col-9">
-                                        <h6>{{ $add->child_info-date_of_decl }}</h6>
+                                        <h6>{{ $item->values->{'child_info-last_name'} }}</h6>
                                     </div>
                                 </div>
 @endsection                              

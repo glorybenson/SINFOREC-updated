a824009482f3f92511d4099ce2019e4a3eb92e6d
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 01:51:28 -0800

    now

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -1,28 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="row">
-        <div class="col-md-10 mx-auto">
-            <div class="mt-3">
-                <div class="content container-fluid">
-                    <div class="page-header">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <div class="d-flex align-items-center">
-                                  
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title float-left text-capitalize">
-                                        {{ $add->get('first_name') . ' ' . $add->get('last_name') }}</h4>
-                                    <div class="text-right">
-                                        <a href="{{ route('registre') }}" class="btn btn-dark px-3">
-                                            Retour</a>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-
                                     <table class="table table-bordered table-striped">
                                         <tr>
                                             <td>Prénom</td>
@@ -59,20 +37,4 @@
                                         </td>
                                     </table>
 
-
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-
-
-    </div>
-    </div>
-    </div>
-    </div>
 @endsection

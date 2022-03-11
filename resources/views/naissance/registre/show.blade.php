7f07a0558a02b2cad1a626d891b98ef00ed367fd
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 16:12:35 -0800

    jhgiui

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -69,7 +69,7 @@
                                         <p>Nom de famille de l'enfant</p>
                                     </div>
                                     <div class="col-9">
-                                        {{$registre -> get('id')}}
+                                        {{$registre -> get('child_info-dob')}}
                                     </div>
                                 </div>
                                 <hr>

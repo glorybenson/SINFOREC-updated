b4462918a9c646f03329bed88b561eeafefce0d5
Glory Benson <glorybenson3738@gmail.com>
2022-03-11 07:11:03 -0800

    djlr

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -31,7 +31,7 @@
                                     <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-right">Bulletin de Naissance</li>
                                     <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-right">Acte de Naissance</li>
                                 </ul>
-                            
+                            <div></div>
 
                                     <h4 class="card-title float-left">Renseignement sur l’enfant</h4>
                             </div>

de8fd84079eaf3040c0c1bcf8f73e63555eeeaa8
Glory Benson <glorybenson3738@gmail.com>
2022-03-10 16:14:55 -0800

    iuyi

--- app/Http/Controllers/Add/AddController.php
+++ app/Http/Controllers/Add/AddController.php
@@ -108,7 +108,7 @@ class AddController extends Controller
         $add = DB::table('naissance_add')
             ->join('users', 'naissance_add.created_by', '=', 'users.id')
             ->select('naissance_add.*', 'users.first_name as admin_first_name', 'users.last_name as admin_last_name')
-            ->get()->first();
+            ->get('child_info-dob')->first();
         return view('naissance.registre.show', ['registre' => collect($add)]);
     }
 

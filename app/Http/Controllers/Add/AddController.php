b9239d7c42f582d7a394e2d922ef292efe9c4dd9
Glory Benson <glorybenson3738@gmail.com>
2022-03-11 11:11:11 -0800

    rrt

--- app/Http/Controllers/Add/AddController.php
+++ app/Http/Controllers/Add/AddController.php
@@ -121,7 +121,7 @@ class AddController extends Controller
         $shell = new \stdClass();
         $binding = Util::load( $shell);
 
-        $binding[ 'naissance_add'] = DB::select( 'SELECT naissance_add.id, arrondissement.description as geographical_zone-arrondissements,
+        $binding[ 'naissance_add'] = DB::select( 'SELECT naissance_add.id, arrondissement.id as geographical_zone-arrondissements,
                                     JOIN geographical_zone-arrondissements ON naissance_add.arrondissements=arrondissement.id'
             . ' WHERE naissance_add.id=?', [$id])[ 0];
 

c39234a3414d4efc36a78e92eb3217046894da1b
Glory Benson <glorybenson3738@gmail.com>
2022-03-12 14:29:36 -0800

    fse

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -85,7 +85,6 @@
                             <div>
                             <div class="accordion" id="accordionExample">
   <div class="card">
-    <div class="card-header" id="headingOne">
         <h5 class="mb-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
         1. Zone Gérographique</h5>
     </div>
@@ -118,7 +117,6 @@
     </div>
   </div>
   <div class="card">
-    <div class="card-header" id="headingTwo">
         <h5 class="mb-0" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
         2. Renseignement sur le Père</h5>
     </div>

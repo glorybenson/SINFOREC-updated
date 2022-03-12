50e18941d5eb9ec186079b77dcf0950c619f58ad
Glory Benson <glorybenson3738@gmail.com>
2022-03-12 14:00:34 -0800

    sacc

--- resources/views/naissance/registre/show.blade.php
+++ resources/views/naissance/registre/show.blade.php
@@ -84,11 +84,8 @@
                             <div class="accordion" id="accordionExample">
   <div class="card">
     <div class="card-header" id="headingOne">
-      <h2 class="mb-0">
-        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        1. Zone Gérographique
-        </button>
-      </h2>
+        <h5 class="mb-0" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        1. Zone Gérographique</h5>
     </div>
 
     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">

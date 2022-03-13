bd80d66697be5e61d9127912536a3f9916bdb6fe
Glory Benson <glorybenson3738@gmail.com>
2022-03-13 09:52:27 +0100

    huuug

--- resources/views/email/newUser.blade.php
+++ resources/views/email/newUser.blade.php
@@ -2,8 +2,8 @@
 Bonjour {{ $user->first_name }} {{ $user->last_name }},<br>
 Nous vous confirmons que votre compte a été crée.
 Votre nom d'utilisateur est {{ $user->email }}
-Le lien pour accéder au portail est:<a href="{{ config('app.url') }}">.
+Le lien pour accéder au portail est:<a href="{{ config('app.url') }}">sinforec.gouvsn.org</a>
 
-Merci
+Merci <br>
 Sinforec Admin
 @endcomponent

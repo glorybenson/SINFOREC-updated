a34028de72098c74f6fdb4196f0fbef9ae020bb8
Glory Benson <glorybenson3738@gmail.com>
2022-03-13 09:35:28 +0100

    fdfgggg

--- resources/views/email/newUser.blade.php
+++ resources/views/email/newUser.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
-Bonjour {{ $user->first_name }} {{ $user->last_name }}
+Bonjour {{ $user->first_name }} {{ $user->last_name }},<br>
 Nous vous confirmons que votre compte a été crée.
-Votre nom d'utilisateur est "username of the user".
+Votre nom d'utilisateur est {{ $user->email }}
 Le lien pour accéder au portail est:<a href="{{ config('app.url') }}">.
 
 Merci

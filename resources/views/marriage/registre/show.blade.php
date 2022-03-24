59b87fb8fe0f69d5916345ebaaf8d7884338b1bb
Glory Benson <glorybenson3738@gmail.com>
2022-03-24 23:30:56 +0100

    Y

--- resources/views/marriage/registre/show.blade.php
+++ resources/views/marriage/registre/show.blade.php
@@ -97,8 +97,7 @@
                                         <div class="card-header" id="headingOne">
                                             <h5 class="mb-0" data-toggle="collapse" data-target="#collapseOne"
                                                 aria-expanded="false" aria-controls="collapseOne">
-                                                1. Zone Gérographique
-                                                <div></div>
+                                                1. Zone Gérographique  
                                                 <span><i class="arrow down"></i></span>
                                             </h5>
                                         </div>

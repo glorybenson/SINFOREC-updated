48282ed4cdb61a630c831c170455045317fe807d
Ore A <oreadeleye67@gmail.com>
2022-03-29 02:25:06 +0100

    deces functionality

--- routes/web.php
+++ routes/web.php
@@ -139,7 +139,7 @@ Route::middleware( 'auth')->group( function () {
     Route::get('/regions/{id}/delete', [App\Http\Controllers\Regions\RegionsController::class, 'destroy'])->name('region.delete');
 
     //Deces Routes Here
-    Route::get('deces/create', [App\Http\Controllers\Deces\DecesController::class, 'createView'])->name('deces.registre');
-    Route::get('deces', [App\Http\Controllers\Deces\DecesController::class, 'index'])->name('deces.index');
-    Route::post('', [App\Http\Controllers\Deces\DecesController::class, 'storeDeces'])->name('deces.store');
+    Route::get('deces/registre/create', [App\Http\Controllers\Deces\DecesController::class, 'createView'])->name('deces.registre');
+    Route::get('deces/registre', [App\Http\Controllers\Deces\DecesController::class, 'index'])->name('deces.index');
+    Route::post('deces/registre/create', [App\Http\Controllers\Deces\DecesController::class, 'storeDeces'])->name('deces.store');
 });

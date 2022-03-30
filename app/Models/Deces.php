7340402e0bdf7a702b07fd9115d9cc23bebe8cb2
Glory Benson <glorybenson3738@gmail.com>
2022-03-30 22:04:36 +0100

    ggghkksooo

--- app/Models/Deces.php
+++ app/Models/Deces.php
@@ -8,10 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Deces extends Model
 {
     use HasFactory;
-
-    class Deces extends Model
-{
-    use HasFactory;
     protected $table = 'deces';
 }
 

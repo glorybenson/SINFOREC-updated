eb960ab4fa5ef7f1159d29d5a20cccbb7dc89b0e
Glory Benson <glorybenson3738@gmail.com>
2022-03-31 21:03:43 +0100

    new

--- resources/views/Deces/registre/show.blade.php
+++ resources/views/Deces/registre/show.blade.php
@@ -310,7 +310,7 @@
                                                     </tr>
                                                     <tr>
                                                         <td>Lieu d'emission</td>
-                                                        <td>{{ $models['judgement-region'] }}</td>
+                                                        <td>{{ $values->{'judgement-region'} }}</td>
                                                     </tr>
                                                 </table>
                                             </div>

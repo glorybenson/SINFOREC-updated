7e9776e0879e1506af1ca3f49e241e1d2262ea80
Glory Benson <glorybenson3738@gmail.com>
2022-04-04 21:31:43 +0100

    Update show.blade.php

--- resources/views/marriage/registre/show.blade.php
+++ resources/views/marriage/registre/show.blade.php
@@ -28,9 +28,10 @@
                             <div class="card">
                                 <div class="card-header">
                                     <div>
-                                        <ul>
-                                            <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'groom-first_name'} }} {{ $values->{'groom-family_name'} }}</li>
-                                            <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'bride-first_name'} }} {{ $values->{'bride-family_name'} }}</li>
+                                        <h5 class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'groom-first_name'} }} {{ $values->{'groom-family_name'} }}</h5>
+                                            <h5 class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'bride-first_name'} }} {{ $values->{'bride-family_name'} }}</h5>
+
+                                        <ul class="text-right">
                                             <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-right">Certificat de
                                                 Mariage
                                             </li>

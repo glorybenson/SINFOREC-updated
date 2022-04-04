9ef84c5eba8c0edc14125e0f7f22bcc1712ce88f
Glory Benson <glorybenson3738@gmail.com>
2022-04-04 23:56:27 +0100

    Update show.blade.php

--- resources/views/Deces/registre/show.blade.php
+++ resources/views/Deces/registre/show.blade.php
@@ -27,7 +27,7 @@
                                 <div class="card-header">
                                     <div class="text-left">
                                         <div class="position-absolute top-50">
-                                            <h4 class="text-primary">
+                                            <h4 class="text-primary ml-2 p-2 pr-3 pl-3">
                                     {{ $values->{'deceased-first_name'} }}
                                      {{ $values->{'deceased-family_name'} }} </h4>
                                     </div>

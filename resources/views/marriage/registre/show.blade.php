0cc3bcf0ec0965e129015547819ea853f70aecd7
Glory Benson <glorybenson3738@gmail.com>
2022-04-04 21:13:47 +0100

    Update show.blade.php

--- resources/views/marriage/registre/show.blade.php
+++ resources/views/marriage/registre/show.blade.php
@@ -28,10 +28,9 @@
                             <div class="card">
                                 <div class="card-header">
                                     <div class="text-left">
-                                        <h5>{{ $values->{'groom-first_name'} }} {{ $values->{'groom-family_name'} }}</h5>
-                                        <h5>{{ $values->{'bride-first_name'} }} {{ $values->{'bride-family_name'} }}</h5>
+                                        <h5 class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'groom-first_name'} }} {{ $values->{'groom-family_name'} }}</h5>
+                                        <h5 class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-left">{{ $values->{'bride-first_name'} }} {{ $values->{'bride-family_name'} }}</h5>
                                     </div>
-                                
 
                                     <div class="text-right">
                                         <ul>

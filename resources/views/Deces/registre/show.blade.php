80c2c0a9e672a607ccec83576b5bcdd1d6e7d840
Glory Benson <glorybenson3738@gmail.com>
2022-04-04 19:54:32 +0100

    Update show.blade.php

--- resources/views/Deces/registre/show.blade.php
+++ resources/views/Deces/registre/show.blade.php
@@ -26,10 +26,13 @@
                             <div class="card">
                                 <div class="card-header">
                                     <div class="text-right">
-                                        <ul>
-                                            <li><h4 class="position-absolute top-0 start-0">
+                                        <ul>                      
+                                            <li>
+                                                <div class="position-relative position-relative-example">
+                                                    <h4 class="position-absolute top-0 start-0">
                                             {{ $values->{'deceased-first_name'} }}
                                              {{ $values->{'deceased-family_name'} }} </h4>
+                                            </div>
                                            </li>
                                             <li class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-right">Bulletin de Décès
                                             </li>

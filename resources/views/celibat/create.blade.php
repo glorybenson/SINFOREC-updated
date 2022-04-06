6623b2b7f8c88c223ae7edbd3bedbfc6b32c5878
Glory Benson <glorybenson3738@gmail.com>
2022-04-06 12:47:08 +0100

    Update create.blade.php

--- resources/views/celibat/create.blade.php
+++ resources/views/celibat/create.blade.php
@@ -36,6 +36,8 @@
 
                                         <div class="row mb-3">
                                             <label for="first_name">Prénom</label>
+
+                                            <div class="col-md-10">
                                             <input id="first_name" type="text" class="form-control
                                             serializable" name="first_name" tabindex="1" data-parsley-errors-container="#invalid-feedback13" required >
                                             <span class="feedback-new" id="invalid-feedback13" role="alert"></span>
@@ -45,9 +47,11 @@
                                         </span>
                                             @endif
                                         </div>
+                                    </div>
 
                                         <div class="row mb-3">
                                             <label for="last_name">Nom de famillee</label>
+                                            <div class="col-md-10">
                                             <input id="last_name" type="text" class="form-control
                                             serializable" name="last_name" tabindex="1" data-parsley-errors-container="#invalid-feedback13" required >
                                             <span class="feedback-new" id="invalid-feedback13" role="alert"></span>
@@ -57,19 +61,23 @@
                                         </span>
                                             @endif
                                         </div>
+                                        </div>
 
 
                                     
                                     <div class="row mb-3">
                                         <label for="dob">
                                             Date de Naissance</label>
+                                            <div class="col-md-10">
                                         <input id="dob" placeholder="Select date" type="date"
                                                name="dob" class="form-control serializable" tabindex="3" data-parsley-errors-container="#invalid-feedback15" required >
                                         <span class="feedback-new" id="invalid-feedback15" role="alert"></span>
                                     </div>
+                                </div>
 
                                     <div class="row mb-3">
                                         <label for="address">Addresse</label>
+                                        <div class="col-md-10">
                                         <input id="address" type="text" class="form-control
                                         serializable" name="address" tabindex="6" data-parsley-errors-container="#invalid-feedback24" required >
                                         <span class="feedback-new" id="invalid-feedback24" role="alert"></span>
@@ -79,9 +87,11 @@
                                     </span>
                                         @endif
                                     </div>
+                                </div>
 
                                     <div class="row mb-3">
                                         <label for="cin" class="required">C.I.N</label>
+                                        <div class="col-md-10">
                                         <input id="cin" type="number" class="form-control serializable" name="cin" tabindex="5" data-parsley-errors-container="#invalid-feedback32" required >
                                         <span class="feedback-new" id="invalid-feedback32" role="alert"></span>
                                         @if ($errors->has('description'))
@@ -90,17 +100,21 @@
                                     </span>
                                         @endif
                                     </div>
+                                </div>
 
                                     <div class="row mb-3">
                                         <label for="demande">
                                             Date de la demande</label>
+                                            <div class="col-md-10">
                                         <input id="demande" placeholder="Select date" type="date"
                                                name="demande" class="form-control serializable" tabindex="3" data-parsley-errors-container="#invalid-feedback15" required >
                                         <span class="feedback-new" id="invalid-feedback15" role="alert"></span>
                                     </div>
+                                </div>
 
                                     <div class="row mb-3">
                                         <label for="declaration_number">Numéro de déclaration</label>
+                                        <div class="col-md-10">
                                         <input id="declaration_number" type="text" class="form-control serializable" name="declaration_number" tabindex="1" data-parsley-errors-container="#invalid-feedback8" required >
                                         <span class="feedback-new" id="invalid-feedback8" role="alert"></span>
                                         @if ($errors->has('description'))
@@ -109,6 +123,7 @@
                                     </span>
                                         @endif
                                     </div>
+                                </div>
 
                                     <div class="text-right">
                                         <button type="submit" class="btn btn-primary px-4" onclick="return confirm('Are you sure you want to submit this form?')">

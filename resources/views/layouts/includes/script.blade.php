4b81064ba7629c950452a5d467089bd14a6298fb
Glory Benson <glorybenson3738@gmail.com>
2022-04-15 18:49:26 +0100

    Update script.blade.php

--- resources/views/layouts/includes/script.blade.php
+++ resources/views/layouts/includes/script.blade.php
@@ -411,7 +411,7 @@ const data = JSON.parse($('[data-tool]').attr('data-tool'));
             }
         }
     }
-    window.addEventListener('load', appendFields(data))
+    appendFields(data)
 
     (function(document){
             var div = document.getElementById('headingOne');

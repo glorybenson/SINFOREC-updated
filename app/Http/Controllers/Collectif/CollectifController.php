46d041862d8628d773dccb40c27718c2ea1a7c2b
Glory Benson <glorybenson3738@gmail.com>
2022-04-15 23:18:56 +0100

    Update CollectifController.php

--- app/Http/Controllers/Collectif/CollectifController.php
+++ app/Http/Controllers/Collectif/CollectifController.php
@@ -123,7 +123,6 @@ final class CollectifController extends Controller
         $shell = new \stdClass();
         $binding = Util::load( $shell);
         $binding[ 'old'] = $old->values;
-        $binding[ 'post_url'] = route( 'collectif.show.post', [ 'id' => $id]);
         $binding[ 'page_url'] = route( 'collectif.show', [ 'id' => $id]);
 
         $binding[ 'is_show'] = true;

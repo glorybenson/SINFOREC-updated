2e98f3680fb5058be3b80a9a2d291290c23e8f30
Glory Benson <glorybenson3738@gmail.com>
2022-04-15 22:53:33 +0100

    Update show.blade.php

--- resources/views/collectif/show.blade.php
+++ resources/views/collectif/show.blade.php
@@ -2,5 +2,75 @@
 
 @section('content')
 
-    <h2> Hello </h2>
+    <div class="row"
+    data-tool="{{ isset( $old) ? $old : '{}' }}"
+     data-conn="{{ $link }}">
+        
+    <!--<p class="hidden">@json ($registre) </p>-->
+        <div class="col-md-10 mx-auto">
+            <div class= "mt-3">
+                <div class="content container-fluid">
+                    <div class="page-header">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
+                                <div class="d-flex align-items-center">
+                                    <h5 class="page-title">Dashboard</h5>
+                                    <ul class="breadcrumb ml-2">
+                                        <li class="breadcrumb-item"><a href="{{ route('collectif.index') }}">Vie Collectif</a>
+                                        </li>
+                                        <li class="breadcrumb-item active">View</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-header">
+
+                                    <div class="text-right">
+                                        <h5 class="btn btn-primary ml-2 p-2 pr-3 pl-3 pull-right">Generer le Certificat de Vie Collectif
+                                        </h5>
+                                    </div>
+                                </div>
+
+                                <div class="card-body">
+                                    <div>
+                                        <h4 class="card-title float-left"></h4>
+                                    </div>
+                                    <table class="table table-bordered table-striped">
+                                        <tr>
+                                            <td>Prénom du demandeur</td>
+                                            <td>{{ $values->{'first_name'} }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Numéro de déclaration</td>
+                                            <td>{{ $values->{'last_name'} }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Prénom du Père</td>
+                                            <td>{{ $values->{'pere_first-name'} }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nom de famille du Père</td>
+                                            <td>{{ $values->{'pere_last-name'} }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Prénom de la Mère</td>
+                                            <td>{{ $values->{'mere_first-name'} }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Nom de famille de la Mère</td>
+                                            <td>{{ $values->{'mere_last-name'} }}</td>
+                                        </tr>
+                                        
+                                    </table>
+
+                                    <div class="mb-5">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 @endsection

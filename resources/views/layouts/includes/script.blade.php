348a8ef2ae88c83e82672b10fa9ffea010fbb5aa
Glory Benson <glorybenson3738@gmail.com>
2022-04-15 19:02:07 +0100

    Update script.blade.php

--- resources/views/layouts/includes/script.blade.php
+++ resources/views/layouts/includes/script.blade.php
@@ -385,33 +385,33 @@
         }
     });
 
-const data = JSON.parse($('[data-tool]').attr('data-tool'));
+    const data = JSON.parse($('[data-tool]').attr('data-tool'));
 
-    function appendFields(data) {
-        var i,
-            keys = Object.keys(data);
-        for (i = 0; i < 6; i++) {
-            delete data[keys[i]];
-        }
-        for (const key in data) {
-            console.log(key, data[key])
-            switch (name) {
-                case "ttype":
-                    var parent = document.getElementById('demo');
-                    var newChildFirstName = '<div class="form-holder"><a class="form-label">Prénom</a> <br><input name="' + key + '" type="text" value="' + data[key] + '" class="form-control serializable" placeholder="Prénom" style="width: 50%;"></div><br>';
-                    parent.insertAdjacentHTML('beforeend', newChildFirstName);
-                    break;
-                case "tname":
-                    var parent = document.getElementById('demo');
-                    var newChildLastName = '<div class="form-holder"><a class="form-label">Nom de famille</a> <br><input name="' + key + '" type="text" value="' + data[key] + '" class="form-control serializable" placeholder="Nom de famille" style="width: 50%; position: relative; padding-right: 15px; padding-left: 15px; box-sizing: border-box;"></div><br>';
-                    parent.insertAdjacentHTML('beforeend', newChildLastName);
-                    break;
-                default:
-                    break;
-            }
+function appendFields(data) {
+    var i,
+        keys = Object.keys(data);
+    for (i = 0; i < 6; i++) {
+        delete data[keys[i]];
+    }
+    for (const key in data) {
+        console.log(key, data[key])
+        switch (name) {
+            case "ttype":
+                var parent = document.getElementById('demo');
+                var newChildFirstName = '<div class="form-holder"><a class="form-label">Prénom</a> <br><input name="' + key + '" type="text" value="' + data[key] + '" class="form-control serializable" placeholder="Prénom" style="width: 50%;"></div><br>';
+                parent.insertAdjacentHTML('beforeend', newChildFirstName);
+                break;
+            case "tname":
+                var parent = document.getElementById('demo');
+                var newChildLastName = '<div class="form-holder"><a class="form-label">Nom de famille</a> <br><input name="' + key + '" type="text" value="' + data[key] + '" class="form-control serializable" placeholder="Nom de famille" style="width: 50%; position: relative; padding-right: 15px; padding-left: 15px; box-sizing: border-box;"></div><br>';
+                parent.insertAdjacentHTML('beforeend', newChildLastName);
+                break;
+            default:
+                break;
         }
     }
-    appendFields(data)
+}
+appendFields(data);
 
     (function(document){
             var div = document.getElementById('headingOne');
